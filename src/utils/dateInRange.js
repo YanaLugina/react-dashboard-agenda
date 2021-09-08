a4e8f712257932e7333084966ade09b991bcbe5d
YanaLugina <yana4961@gmail.com>
2021-09-09 01:07:02 +1000

    filterLayers: Was created filter for picked layers
            изменено:      CHANGELOG.md
            изменено:      example/.eslintcache
            изменено:      example/src/App.js
            изменено:      src/components/ChunkItem/ChunkItem.js
            изменено:      src/components/ChunksArea/ChunksArea.js
            изменено:      src/utils/dateInRange.js

--- src/utils/dateInRange.js
+++ src/utils/dateInRange.js
@@ -15,7 +15,7 @@ function dateInRange(
   }
 
   let allow = false
-  let objAllow = { isChecked: false, type: '', data: {} }
+  let objAllow = { isBusy: false, type: '', data: {} }
 
   for (let i = 0; i < arrAllowTimeRanges.length; i++) {
     if (
@@ -37,7 +37,7 @@ function dateInRange(
           ) !== -1
         )
         if (allow) {
-          objAllow = { isChecked: true, ...arrAllowTimeRanges[i] }
+          objAllow = { isBusy: true, ...arrAllowTimeRanges[i] }
         }
       }
     }
